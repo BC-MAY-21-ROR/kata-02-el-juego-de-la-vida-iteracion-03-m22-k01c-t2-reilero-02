e79e0dcab81be84672c7a179664bd223865c0cb1
OwenLobato <owenlobatov@gmail.com>
2022-05-12 17:58:27 -0500

    Try rspec with chomps Co-authored-by d1eg0dev
    <diego@studiowd.com.mx>
    Co-authored-by EdwardMD Edwardmi444@gmail.com
    Co-authored-by ruruisu <zaddozgl@gmail.com>

--- board.rb
+++ board.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
+require'matrix'
 class Board
+  
+  def values
+    print "Elige alto del tablero: "
+    $row=gets.chomp.to_i
+    print "Elige ancho del tablero: "
+    $column=gets.chomp.to_i
+  end
+
   def create_board
-    puts []
+    matriz = Matrix.build($row, $column) { rand(2) }
   end
 end
--- tests.rb
+++ tests.rb
@@ -5,9 +5,22 @@ require_relative './cells'
 require_relative './board'
 
 describe Board do
+
+  describe '#values_row' do
+    it 'asf for row it returns integer' do
+      expect(Board.new().values.$row).to eql(3)
+    end
+  end
+
+  describe '#values_column' do
+    it 'ask form column it returns integer' do
+      expect(Board.new().values.$column).to eql(3)
+    end
+  end
+
   describe '.create_board' do
     it 'exists' do
-      expect(Board.new().create_board.class)=Array
+      expect(Board.new().create_board.class).to eql(Matrix)
     end
   end
 end
